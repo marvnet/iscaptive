adfbe20161686bd5f6a34393df5741dec5f17a9d
MagicMarvMan <marvin.kai@gmx.de>
2019-07-13 21:35:25 +0200

    Define captive detection portals

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 const fetch = require("node-fetch")
+
+// define possible captive detection server
+const captives = {
+    "marvnet": "detectcaptive.marvnet.digital",
+    "apple": "captive.apple.com",
+}
