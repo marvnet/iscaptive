fd6473fa7b419ad655333714f3215d477ce9a9f3
MagicMarvMan <marvin.kai@gmx.de>
2019-07-13 21:37:31 +0200

    Remove apple captive portal

--- src/index.js
+++ src/index.js
@@ -3,5 +3,4 @@ const fetch = require("node-fetch")
 // define possible captive detection server
 const captives = {
     "marvnet": "detectcaptive.marvnet.digital",
-    "apple": "captive.apple.com",
 }
