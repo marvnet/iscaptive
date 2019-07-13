f7c1ea0369d5c73a8fb09d0c921003367103aa15
MagicMarvMan <marvin.kai@gmx.de>
2019-07-13 22:06:49 +0200

    Don't set as string attribute

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const fetch = require("node-fetch")
 
 // define possible captive detection server
 const captives = {
-    "marvnet": "iscaptive.marvnet.digital",
+    marvnet: "iscaptive.marvnet.digital",
 }
 
 // define detection algorithms for detection pages
 const detectMarvnet = (protocol) => {
     fetch(protocol + "://" + captives.marvnet)
         .then((response) => {
-            
+
         })
 }
