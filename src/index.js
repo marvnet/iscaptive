f9bc9e446e1e3e09dc2a680b3e9263780744f723
MagicMarvMan <marvin.kai@gmx.de>
2019-07-14 13:32:00 +0200

    Use promise

--- src/index.js
+++ src/index.js
@@ -5,32 +5,28 @@ const captives = {
     marvnet: "iscaptive.marvnet.digital",
 }
 
-const detectMarvnet = (protocol) => {
-    request(protocol + "://" + captives.marvnet, (error, response, body) => {
-        if(error) {
-            return true
+module.exports = (server, protocol) => {
+    return new Promise((resolve, reject) => {
+        server = typeof server  !== 'undefined' ?  server  : "marvnet"
+        protocol = typeof protocol !== 'undefined' ? protocol : "https"
+        if(server == "marvnet") {
+            request(protocol + "://" + captives.marvnet, (error, response, body) => {
+                    if(error) {
+                        resolve(true)
+                    } else {
+                        if(response.statusCode == 200) {
+                            if(body == "Success") {
+                                resolve(false)
+                            } else {
+                                resolve(true)
+                            }
+                        } else {
+                            resolve(true)
+                        }
+                    }
+                })
         } else {
-            if(response.statusCode == 200) {
-                if(body == "Success") {
-                    return false
-                } else {
-                    return true
-                }
-            } else {
-                return true
-            }
+            resolve(true)
         }
     })
-}
-
-module.exports = (server, protocol) => {
-    server = typeof server  !== 'undefined' ?  server  : "marvnet"
-    protocol = typeof protocol !== 'undefined' ? protocol : "https"
-    switch(server) {
-        case "marvnet":
-            return detectMarvnet(protocol)
-
-        default:
-            return detectMarvnet(protocol)
-    }
 }
